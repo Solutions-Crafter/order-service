61277c0faac201432c56dcad7f77be4e17698381
Channa Bandara <164510515+SolutionsCrafter@users.noreply.github.com>
2026-06-26 14:01:49 +0530

    feat: add swagger API documentation

--- src/main/java/com/ecommerce/order_service/OrderServiceApplication.java
+++ src/main/java/com/ecommerce/order_service/OrderServiceApplication.java
@@ -1,13 +1,18 @@
 package com.ecommerce.order_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "Order Service API",
+		version = "1.0",
+		description = "Handles order creation and communicates with Product Service via REST and RabbitMQ"
+))
 public class OrderServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
-
 }
