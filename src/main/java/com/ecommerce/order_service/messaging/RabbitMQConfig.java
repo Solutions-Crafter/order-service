82f85066bd7f6038e465578424bd7cfde96f1f97
Channa Bandara <164510515+SolutionsCrafter@users.noreply.github.com>
2026-06-26 13:21:13 +0530

    Update

--- src/main/java/com/ecommerce/order_service/messaging/RabbitMQConfig.java
+++ src/main/java/com/ecommerce/order_service/messaging/RabbitMQConfig.java
@@ -1,6 +1,9 @@
 package com.ecommerce.order_service.messaging;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +15,17 @@ public class RabbitMQConfig {
     @Bean
     public Queue orderQueue() {
         return new Queue(ORDER_QUEUE, true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
     }
 }
